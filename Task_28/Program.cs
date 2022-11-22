425d2eddb491072822ea9fede572906176fd9b68
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-22 22:33:37 +0300

    added Task 28

---
+++ Task_28/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N
+int number = UserInput("Введите целое число: ", "Ошибка ввода!");
+Console.WriteLine($"{number} -> {MultN(number)}");
+
+
+
+
+//-----------Ввод данных
+int UserInput(string message, string errorMessage)
+{    
+     while(true)
+    {
+        Console.Write(message);
+        if( int.TryParse(Console.ReadLine(), out int userNumber))
+
+            return userNumber;
+        Console.WriteLine(errorMessage);
+     } 
+}
+
+//------------Умножение
+
+int MultN(int number)
+{
+     int mult = 1;
+     for(int i=1; i <= number; i++)
+    
+         mult *= i;
+    
+     return mult;
+}
