8b62eb908bec67c939a92a8f71b0042bce6c25e7
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-22 22:33:26 +0300

    added Task 26

---
+++ Task_26/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 26 Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+int number = UserInput("Введите целое число: ", "Ошибка ввода!");
+Console.WriteLine($"{number} -> {countN(number)}");
+
+//-------------------Methods
+
+//-----------Ввод данных
+int UserInput(string message, string errorMessage)
+{    
+     while(true)
+    {
+        Console.Write(message);
+        if( int.TryParse(Console.ReadLine(), out int userNumber))
+
+            return userNumber;
+        Console.WriteLine(errorMessage);
+     } 
+}
+
+//-------------Число цифр
+
+int countN(int number)
+{
+    int i=0;
+    do
+    {
+        i++;
+        number = number/10;
+    }
+    while(number!=0);
+    return i;
+}
