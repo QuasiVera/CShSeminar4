4249873f59636c47109ac6fde59ce14168299dcf
VeraBu <opilki-in-yhe-head@mail.ru>
2022-11-25 22:25:05 +0300

    added Seminar_5

---
+++ Seminar_5/Task_35/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов
+// массива, значения которых лежат в отрезке [10,99]
+
+Console.Clear();
+int[] array = GetArray(123, -200, 200);
+PrintArray(array);
+Console.WriteLine(String.Join(" ", array));
+int countPositiveEl = CountPositiveEl(array, 10, 99);
+
+
+Console.WriteLine($"Count of positive = {countPositiveEl} ");
+//////////////////////////////////////////////////////////////////////////////////
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+// Возвращает число положительных чисел массива arr
+int CountPositiveEl(int[] arr, int min, int max)
+{
+    int countPositiveEl = 0;
+    foreach (int el in arr)
+    {
+        if (el >= min && el<=max) countPositiveEl += 1;
+    }
+    return countPositiveEl;
+}
+
+//-----------Печатает массив
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write($"]");
+}
---
+++ Seminar_5/Task_37/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+int size = new Random().Next(3, 7);
+int[] array = GetArray(size, -10, 10);
+PrintArray(array);
+int[] result = PairMultiplication(array);
+PrintArray(result);
+
+//--------------Генерируем массив
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+//-----------Печатает массив
+void PrintArray(int[] array)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write($"]");
+}
+
+//--------------------Умножаем парами
+int[] PairMultiplication(int[] array)
+{
+    int size = 0;
+    if(array.Length % 2 == 0)
+
+    {
+        size = array.Length / 2;
+    }
+        
+    else
+    {
+        size = array.Length / 2 + 1;   
+    }
+    int [] res = new int[size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        if(array[1]!=array[array.Length-1-i])  // Чтобы не умножал сам на себя
+            res[i] = array[i]*array[array.Length-1-i];
+        else
+            res[i] = array[i];
+    }
+    return res;
+}
