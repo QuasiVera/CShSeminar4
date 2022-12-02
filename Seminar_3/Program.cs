a0e75f543e23c162b98cfe0b4e9f532128b4aef5
VeraBu <opilki-in-yhe-head@mail.ru>
2022-12-02 23:32:25 +0300

    corrected Seminar_3 ln 5

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -2,7 +2,7 @@
 //и выдаёт номер четверти плоскости, в которой находится эта точка.
 Console.Clear();
 Console.Write("Введите X: ");
-int X = int.Parse(Console.ReadLine());
+int X = int.Parse(Console.ReadLine() ?? " ");
  Console.Write("Введите Y: ");
 int Y = int.Parse(Console.ReadLine());
  if(X > 0 && Y > 0)
